564ce3457d6742141cd7c412bc43a06453a76f97
TinaCXu <xuchangtina@163.com>
2020-05-11 02:15:32 +0800

    vue-demo

--- hello-ui/src/views/About.vue
+++ hello-ui/src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>{{msg}}</p>
   </div>
 </template>
+<script>
+import Store from '@/store/index'
+export default {
+  name: 'about',
+  Store,
+  data () {
+    return {
+      msg: Store.state.count
+    }
+  }
+}
+</script>
---
+++ hello-ui/src/views/Info.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="about">
+    <h1> Hello Info Component</h1>
+    <button type="button" @click="add()">ADD</button>
+  </div>
+</template>
+<script>
+import Store from '@/store/index'
+export default {
+  name: 'Info',
+  Store,
+  mounted () {
+    window.vue = this
+  },
+  data () {
+    return {
+      msg: 'hello vue'
+    }
+  },
+  methods: {
+    add () {
+      console.log('add Event from info!')
+      //  console.error('INFO!')
+      //  添加debugger断点进行调试
+      debugger
+      Store.commit('increase')
+    },
+    output () {
+      console.log('OUTPUT')
+    }
+  }
+}
+</script>
